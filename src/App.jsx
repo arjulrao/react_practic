a5627f7474490a0242d8c2ee1c5dd7ed473a1d00
arjulrao <arjulrao21@gmail.com>
2025-09-18 14:13:03 +0530

    Create Card with the help of map

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,12 @@ import Card from './Components/Card';
 
 function App() {
   
-  const data = ['Harsh', "Ankur", "Moin", "Sharthak", "Muskaan", "Satwik"]
-
-    return(
-      <div>
-        {data.map((elem, index)=>(
-           <div key={index} className='px-3 py-4 bg-zinc-300 rounded-md w-fit' >
-              { elem }
-            </div>)
-        )}  
-      </div>
-         
+    return (
+       <div>
+          <Card />
+       </div>  
    ) 
+
 }
 
 export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,18 +2,38 @@ import React from 'react'
 
 
 const Card = () => {
-  return (
-    <div className='w-full h-screen bg-zinc-200'>
-      <div className='w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden'>
-      <div className='w-full h-32 bg-zinc-300'>
-        <img className='w-full h-full object-cover' src="https://plus.unsplash.com/premium_photo-1681488262364-8aeb1b6aac56?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-      </div>
-      <div className='w-full px-3 py-4'>
-        <h2 className='font-semibold'>Amazon Basics.</h2>
-        <p className='text-xs mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque excepturi dignissimos nobis?</p>
-      </div>
+  const product = [
+    { 
+      image: `https://plus.unsplash.com/premium_photo-1681488262364-8aeb1b6aac56?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+      name: "Amazon Basics",
+      description: 'adipisicing elit. Neque excepturi dignissimos nobis?'},
+    {
+      image: `https://images.unsplash.com/photo-1615725802642-936d9aade2ba?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+      name: "Apple Store",
+      description: 'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+      image: `https://images.unsplash.com/photo-1725085662412-14bd6f48a76b?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+      name: "Samsung Store",
+      description: 'Lorem ipsum dolor t. Neque excepturi dignissimos nobis?'
+    }
+  ]
 
-      </div>
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center gap-10'>
+      {
+        product.map((elem, index) => (
+          <div key={index} className='w-52 bg-zinc-100  rounded-md overflow-hidden'>
+            <div className='w-full h-32 bg-zinc-300'>
+              <img className='w-full h-full object-cover' src= {elem.image} alt="" />
+            </div>
+            <div className='w-full px-3 py-4'>
+              <h2 className='font-semibold'> {elem.name} </h2>
+              <p className='text-xs mt-5'> {elem.description}</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
