0accd9082427ad1ea62689e91342aee3001a0a61
arjulrao <arjulrao21@gmail.com>
2025-09-18 13:42:31 +0530

    Daynamic value change

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 import Card from './Components/Card';
 
 function App() {
+  
+  const data = ['Harsh', "Ankur", "Moin", "Sharthak", "Muskaan", "Satwik"]
 
     return(
-      <>
-        <Card />
-      </>
+      <div>
+        {data.map((elem, index)=>(
+           <div key={index} className='px-3 py-4 bg-zinc-300 rounded-md w-fit' >
+              { elem }
+            </div>)
+        )}  
+      </div>
          
    ) 
-
-
 }
 
 export default App;
