0bfd41a372c6d2358a03ea821e4442637ad81b3b
arjulrao <arjulrao21@gmail.com>
2025-09-20 12:32:07 +0530

    Condition Rendering

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,16 +6,20 @@ const Card = () => {
     { 
       image: `https://plus.unsplash.com/premium_photo-1681488262364-8aeb1b6aac56?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
       name: "Amazon Basics",
-      description: 'adipisicing elit. Neque excepturi dignissimos nobis?'},
+      description: 'adipisicing elit. Neque excepturi dignissimos nobis?',
+      instock: true
+    },
     {
       image: `https://images.unsplash.com/photo-1615725802642-936d9aade2ba?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
       name: "Apple Store",
-      description: 'Lorem ipsum dolor sit amet consectetur.'
+      description: 'Lorem ipsum dolor sit amet consectetur.',
+      instock: false
     },
     {
       image: `https://images.unsplash.com/photo-1725085662412-14bd6f48a76b?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
       name: "Samsung Store",
-      description: 'Lorem ipsum dolor t. Neque excepturi dignissimos nobis?'
+      description: 'Lorem ipsum dolor t. Neque excepturi dignissimos nobis',
+      instock: true
     }
   ]
 
@@ -30,6 +34,7 @@ const Card = () => {
             <div className='w-full px-3 py-4'>
               <h2 className='font-semibold'> {elem.name} </h2>
               <p className='text-xs mt-5'> {elem.description}</p>
+              <button className={`px-4 py-1 ${elem.instock ? "bg-blue-600" : "bg-red-600 "} text-xs rounded text-zinc-100 mt-3`} >{elem.instock ? "In Stock" : "Out Of Stock"}</button>
             </div>
           </div>
         ))
